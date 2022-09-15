9853c8efa91a5fd68cf51149828388bf0e6ef67d
munnsmunns <mmunns16@gmail.com>
2022-09-15 13:10:51 -0400

    modify to be a multi page app

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
-from dash import Dash, Input, Output, dcc, html
+import dash
+from dash import Dash, dcc, html
 
-app = Dash(__name__)
+app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "x: ",
-        x_input := dcc.Input(type='number')
-    ]),
-    html.Div([
-        "y: ",
-        y_input := dcc.Input(type='number')
-    ]),
-    html.Br(),
-    output := html.Div(),
+	html.H1('Multi-page app with Dash Pages'),
 
+    html.Div(
+        [
+            html.Div(
+                dcc.Link(
+                    f"{page['name']} - {page['path']}", href=page["relative_path"]
+                )
+            )
+            for page in dash.page_registry.values()
+        ]
+    ),
+
+	dash.page_container
 ])
-
-
-@app.callback(
-    Output(output, component_property='children'),
-    Input(x_input, component_property='value'),
-    Input(y_input, component_property='value')
-)
-def update_output_div(x_input, y_input):
-    if x_input and y_input:
-        return f'x * y = {x_input * y_input}'
-    return html.H6("Both inputs are required.", style={"color":"red"})
 
 
 if __name__ == '__main__':
