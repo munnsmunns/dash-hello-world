693475e5cbb62c3d33987f0e10c44c1fb0a67ec8
munnsmunns <mmunns16@gmail.com>
2022-09-15 13:45:38 -0400

    add navbar

--- app.py
+++ app.py
@@ -4,19 +4,17 @@ from dash import Dash, dcc, html
 app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
-	html.H1('Multi-page app with Dash Pages'),
-
-    html.Div(
-        [
-            html.Div(
-                dcc.Link(
-                    f"{page['name']} - {page['path']}", href=page["relative_path"]
-                )
+    html.Nav(
+        html.Div(
+            html.Ul([
+                html.Li(dcc.Link(
+                    f"{page['name']}", href=page["relative_path"], className="nav-link active"
+                ), className="nav-item"
             )
             for page in dash.page_registry.values()
-        ]
+            ], className="navbar-nav me-auto mb-2 mb-lg-0"), className="container-fluid"
+        ), className="navbar navbar-expand-lg bg-light"
     ),
-
 	dash.page_container
 ])
 
